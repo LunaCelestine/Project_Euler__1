8a6830883ffce9e01fb759fa16ce3cfd6ca9d07a
bdabdoub <bdabdoub@gmail.com>
2019-05-20 09:14:45 -0400

    Solved

---
+++ EulerProject_1.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 18 16:48:29 2019
+
+@author: AdrielleTheSlayer
+"""
+
+print("Number of Processers:", mp.cpu_count())
+
+max = 1000
+
+def count_by_three(max):
+    i = 3
+    results = []
+    while i < max:
+        results.append(i)
+        i += 3
+    return results
+
+def count_by_five(max):
+    k = 5
+    results = []
+    while k < max:
+        results.append(k)
+        k += 5
+    return results
+ 
+
+three = count_by_three(max)
+five = count_by_five(max)     
+quotients = set(count_by_three(max)) | set(count_by_five(max))
+total = sum(quotients)
+print(total)
+
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Project_Euler__1
+
+This program was written for use in the Project Euler challenges.
+
+Link to their website : https://projecteuler.net/about
+
+This specs for this particular challenge are as follows:
+
+"If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000."
+
+Link: https://projecteuler.net/problem=1
+
+
